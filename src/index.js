69b5c727a46d0dcd9b3e1c050ca93a83a8c2ed5c
Chetan Talwar <chetan.talwar@gmail.com>
2020-08-14 14:42:36 +0530

    States Added

--- src/index.js
+++ src/index.js
@@ -6,10 +6,24 @@ import * as serviceWorker from './serviceWorker';
 
 const firstelement = <h1>Hello From Element </h1>
 class Msg extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      name: "Chetan",
+      city: "ABC",
+      profession: "Business"
+    };
+  }
   render(){
-    return <h2>Hello from Class</h2>
+    return ( 
+      <div>
+       <h2>Hello from {this.state.name}</h2>
+       <p>{this.state.city}</p>
+       </div>
+  );
   }
 }
+
 function Msg2() {
   return <h2>Hello from Function</h2>
 }
