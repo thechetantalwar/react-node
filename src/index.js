f4dd2674d1834dd9e99f7bdf2b452fd3cbdd1ffb
Chetan Talwar <chetan.talwar@gmail.com>
2020-08-14 13:18:27 +0530

    Class Function Explained

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firstelement = <h1>Hello From Element </h1>
+class Msg extends React.Component{
+  render(){
+    return <h2>Hello from Class</h2>
+  }
+}
+function Msg2() {
+  return <h2>Hello from Function</h2>
+}
 
+ReactDOM.render(<Msg />,document.getElementById('root'))
+ReactDOM.render(firstelement,document.getElementById('element'))
+ReactDOM.render(<Msg2 />, document.getElementById('func'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
